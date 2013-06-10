34465b2e62c2935d2c97fed7e742360efd9c94fb
Neal Howarth <neal@ripepixel.co.uk>
2013-06-10 22:51:18 +0100

    all appointmets

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,8 +2,14 @@ class AppointmentsController < ApplicationController
   before_filter :authenticate_admin_user!
   
   def index
-  	@appointments = Appointment.where('admin_user_id = ? AND appointment_time between ? AND ?', current_admin_user.id, Date.today.beginning_of_day, Date.today.end_of_day).order('appointment_time ASC')
-  	@future = Appointment.where('admin_user_id = ? AND appointment_time > ? AND appointment_time < ?', current_admin_user.id, Date.today+1.day, Date.today+5.days).limit(10)
+    if current_admin_user.auth_level == "full"
+      @appointments = Appointment.where('appointment_time between ? AND ?', Date.today.beginning_of_day, Date.today.end_of_day).order('appointment_time ASC')
+    	@future = Appointment.where('appointment_time > ? AND appointment_time < ?', Date.today+1.day, Date.today+5.days).order('appointment_time ASC').limit(10)
+    else
+      @appointments = Appointment.where('admin_user_id = ? AND appointment_time between ? AND ?', current_admin_user.id, Date.today.beginning_of_day, Date.today.end_of_day).order('appointment_time ASC')
+    	@future = Appointment.where('admin_user_id = ? AND appointment_time > ? AND appointment_time < ?', current_admin_user.id, Date.today+1.day, Date.today+5.days).order('appointment_time ASC').limit(10)
+    end
+  	
   end
 
   def show
@@ -16,7 +22,7 @@ class AppointmentsController < ApplicationController
 
   def create
   	@appointment = Appointment.new(params[:appointment])
-  	@appointment.admin_user_id = current_admin_user.id
+  	# @appointment.admin_user_id = current_admin_user.id
 
   	if @appointment.save
   		redirect_to appointments_path, notice: "The appointment has been saved"
